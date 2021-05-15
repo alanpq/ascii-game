835c426aa19b40288d00b6c7740a9a6a29c7df87
Alan Panayotov <alanpanayotov@gmail.com>
2021-05-15 21:37:20 +0100

    improved key input

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,9 @@ extern crate pancurses;
 
 mod renderer;
 
-use pancurses::{ALL_MOUSE_EVENTS, Window, endwin, newwin, getmouse, initscr, mousemask, Input, resize_term, REPORT_MOUSE_POSITION, ToChtype, Attribute, curs_set, cbreak, noecho, chtype};
+use pancurses::{ALL_MOUSE_EVENTS, Window, endwin, newwin, getmouse, initscr, mousemask, Input, resize_term, REPORT_MOUSE_POSITION, ToChtype, Attribute, curs_set, cbreak, noecho, chtype, BUTTON1_RELEASED};
 use crate::renderer::curses::CursesRenderer;
 use crate::renderer::{Renderer, drawer::Drawer};
-
 
 fn main() {
     let mut renderer = CursesRenderer::new();
@@ -15,20 +14,23 @@ fn main() {
     renderer.printw("Click in the terminal, press q to exit\n");
     renderer.refresh();
 
+    let mut paused = false;
+
+
     let mut x0 = 0;
     let mut y0 = 0;
-    let mut x1 = 0;
-    let mut y1 = 0;
     let mut toggle = false;
 
     let mut mx = 0;
     let mut my = 0;
-    let mut mc:chtype = 'd' as chtype;
+    let mut mc:chtype = ' ' as chtype;
+
+    let mut old_m = BUTTON1_RELEASED;
 
     loop {
         match renderer.getch() {
             Some(Input::KeyResize) => {
-                resize_term(0,0);
+                resize_term(0, 0);
             }
             Some(Input::KeyMouse) => {
                 if let Ok(mouse_event) = getmouse() {
@@ -38,11 +40,12 @@ fn main() {
                     mc = renderer.mvinch(my, mx);
                     renderer.mvaddch(mouse_event.y, mouse_event.x, 'O');
 
-                    renderer.mvprintw(1, 0,
-                      &format!("Mouse at {},{}\n{}", mouse_event.x, mouse_event.y, mouse_event.bstate),
-                    );
+                    if mouse_event.bstate != old_m {
+                        println!("transition from mouse {} to mouse {}", old_m, mouse_event.bstate);
+                        old_m = mouse_event.bstate;
+                    }
 
-                    if !(mouse_event.bstate == 4 || mouse_event.bstate == 1)  {
+                    if mouse_event.bstate & BUTTON1_RELEASED != BUTTON1_RELEASED {
                         continue;
                     }
                     toggle = !toggle;
@@ -51,16 +54,48 @@ fn main() {
                         x0 = mouse_event.x;
                         y0 = mouse_event.y;
                     } else {
-                        x1 = mouse_event.x;
-                        y1 = mouse_event.y;
-                        renderer.draw_line(x0, y0, x1, y1);
+                        renderer.draw_line(x0, y0, mouse_event.x, mouse_event.y);
                     }
-
                 };
             }
-            Some(Input::Character(x)) if x == 'q' => break,
-            _ => (),
+            Some(Input::Character(chr)) => {
+                println!("chr {} -> {}",chr, chr as u32);
+                match chr as u32 {
+                    113 => break, // q
+                    27 => { // escape
+                        paused = !paused;
+                    },
+                    119 => { // w
+
+                    },
+                    115 => { // s
+
+                    },
+                    97 => { // a
+
+                    },
+                    100 => { // d
+
+                    },
+                    _ => {},
+                }
+            },
+            Some(Input::Unknown(x)) => {
+                println!("unknown {}", x);
+            },
+            asd => {
+                if asd.is_some() {
+                    println!("{:?}", asd);
+                }
+            },
         }
+
+        renderer.erase();
+
+        if paused {
+            renderer.mvaddstr(0,0,"PAUSED");
+        }
+
     }
-    endwin();
+    renderer.kill();
 }
